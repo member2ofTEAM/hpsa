0404ebdc22ef24dbf0e269ff61b1f4eafccaea52
member2ofTEAM <cp1995@nyu.edu>
2013-09-13 12:14:43 -0700

    asdf

--- a2/main.py
+++ a2/main.py
@@ -1,1 +1,14 @@
-ohcbngnslndnakslndaksdnaklsdanksl
+from numpy import genfromtxt
+from scipy.sparse import csr_matrix
+from scipy.spatial.distance import cdist
+from scipy.sparse.csgraph import minimum_spanning_tree
+
+my_data = genfromtxt('C:\\Users\\Sven\\Desktop\\travelingtest.csv', delimiter=' ', usecols = (1,2,3)) #read in data from csv file 
+
+Y = cdist(my_data,my_data, 'euclidean') #create distance matrix
+
+X = csr_matrix(Y)
+Tcsr = minimum_spanning_tree(X) #calculate minimum spanning tree
+#Tcsr.toarray().astype(int)
+
+
