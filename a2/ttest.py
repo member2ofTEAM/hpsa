ba051b829e24bf0b8a6f6545687fe7ec2a19c5aa
member2ofTEAM <cp1995@nyu.edu>
2013-09-20 09:46:05 -0700

    asdf

--- a2/ttest.py
+++ a2/ttest.py
@@ -1,16 +1,18 @@
-import numpy as np
-import networkx as nx
-import time
-import subprocess
-
+from numpy import matrix
 from numpy import genfromtxt
+from sets import Set
+from networkx import Graph
+from networkx import minimum_spanning_tree
+from networkx import MultiGraph
+from networkx import eulerian_circuit
+from subprocess import call
+from sys import argv
 from scipy.spatial.distance import cdist
 
-start = time.time()
 my_data = genfromtxt('travelingtest', delimiter=' ', usecols = (1,2,3)) #read in data from csv file 
 Y = cdist(my_data,my_data, 'euclidean') #create distance matrix
-G = nx.Graph(np.matrix(Y))
-T = nx.minimum_spanning_tree(G)
+G = Graph(matrix(Y))
+T = minimum_spanning_tree(G)
 
 #MINMAXMATCHING
 d = T.degree() #Get dictionary of node:degree
@@ -28,40 +30,33 @@ for n in N:
 	f.write("\n")
 f.close()
 
-print "done writing"
-
-fh = open("NUL","w")
-subprocess.call(["./blossom4","-3", "-x","data.dat", "-w", "output.dat"], stdout=fh)
+fh = open("trash","w")
+call(["./blossom4","-3", "-x","data.dat", "-w", "output.dat"], stdout=fh)
 fh.close()
-
-print "done pipiing"
 
 result = genfromtxt('output.dat', delimiter=' ', usecols = (0, 1))
 result = result[1:]
 #MINMAXMATCHING END
 
-H = nx.MultiGraph()
+H = MultiGraph()
 H.add_nodes_from(T.nodes())
 H.add_edges_from(T.edges())
 for (key, value) in a.items():
 	if N < N:
 		H.add_edge(N, N)
 
-t = []
-seen = set()
-for (i, j) in nx.eulerian_circuit(H):
-	if i in seen:
+t = Set()
+for (i, j) in eulerian_circuit(H):
+	if i in t:
 		continue
-	seen.add(i)
-	t.append(i)
+	t.add(i)
+	print i
 
-print time.time() - start
-	
 #the result is t
-t_e = zip(t[:-1], t[1:])
-distance = 0
-for (i, j) in t_e:
-	distance += Y[i, j]
+#t_e = zip(t[:-1], t[1:])
+#distance = 0
+#for (i, j) in t_e:
+#	distance += Y[i, j]
 	
 
 
