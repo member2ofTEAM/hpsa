c8ef7ee27cd98e8ab76c2178f988b308517d609f
member2ofTEAM <cp1995@nyu.edu>
2013-09-23 11:09:55 -0700

    a3added

---
+++ a3/phase2.py
@@ -0,0 +1,18 @@
+def update_torque(board, torque):
+	#Update torques based on board
+	return 0
+	
+def game_over(torque):
+	return torque[0]*torque[1] > 0
+
+
+
+
+if __name__ == "__main__":
+	board = 31 * [0]
+	board[-4] = 3
+	torque = [0, 0]
+
+
+
+
