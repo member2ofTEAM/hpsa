f96c22454cbe96aed8cf64f76953917eeb3c0cbd
SvenAG <sven.giesselbach@gmail.com>
2013-09-28 23:34:42 -0400

    New Game version - taking board.txt and parameters Test that works

--- a3/Test.py
+++ a3/Test.py
@@ -5,38 +5,60 @@ Created on 25.09.2013
 
 Just a small test utility to initialize a game and make some moves
 '''
-from Game_clean import NoTipping
-from Game_clean import alphabeta_search
+from Game_optimized import NoTipping
+from Game_optimized import alphabeta_search
 import random
 import pdb
+import sys
 
 A = NoTipping()
+A.get_input(int(sys.argv[1]),int(sys.argv[2]))
+A.display()
+print A.to_move
+move = A.magic_alphabeta_search()
+print move
+A = A.make_move(move)
 A.display()
 
 #c = alphabeta_full_search(b,A)
 #print c
+'''
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+'''
 
+print A.to_move
+print A.phase
 
-pdb.set_trace()
+'''
+#pdb.set_trace()
 while True:
     if A.to_move == 1:
 #        pos = int(raw_input("Enter Position: "))
 #        wei = int(raw_input("Enter Weight: "))
-        ((pos, wei), score) = alphabeta_search(A, d=1)
-        move = (pos,wei)
+        #((pos, wei), score) = alphabeta_search(A, d=1)
+        #move = (pos,wei)
+        move = A.magic_alphabeta_search()
         A = A.make_move(move)
         A.display()
         if A.board.tipped():
             break
     else:
-        if A.board.tipped():
-            break
+#        if A.board.tipped():
+#            break
 #        ((pos, wei), score) = alphabeta_search(A, d=1)
-        pos = int(raw_input("Enter Position: "))
-        wei = int(raw_input("Enter Weight: "))
-        move = (pos, wei)
+        #((pos, wei), score) = alphabeta_search(A, d=1)
+        #move = (pos,wei)
+        move = A.magic_alphabeta_search()
         A = A.make_move(move)
         A.display()
+        if A.board.tipped():
+            break
+        #pos = int(raw_input("Enter Position: "))
+        #wei = int(raw_input("Enter Weight: "))
+        #move = (pos, wei)
+        #A = A.make_move(move)
+        #A.display()
         
         
 #        random.random()
@@ -45,10 +67,11 @@ while True:
 #        A.display()
         
         
-        if A.board.tipped():
-            break
+#        if A.board.tipped():
+#            break
 
 if(A.to_move==1):
-    print "Player wins"
+    print "Player "+str(A.to_move)+" wins"
 else:
-    print "Computer wins"
+    print "Player "+str(A.to_move)+" wins"
+'''
