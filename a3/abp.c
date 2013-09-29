f26883cdac623a12398c0465939216594851f18e
memberofTEAM <waefz@hotmail.com>
2013-09-28 19:58:07 -0700

    nightmaaares

--- a3/abp.c
+++ a3/abp.c
@@ -9,20 +9,33 @@ int t0 = -9;
 int t1 = -3;
 int player;
 int board[31];
-int inf = 999999999999999;
+int inf = 999999999;
 int p1w[7];
 int p2w[7];
 
-int d = 4;
+int d = 1;
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+
+/* Player 2 is below 20, Player 1 is above 20 within the nmove */
 
 int main(int argc, char *argv[])
 {
     int i;
-    phase = argv[1];
-    player = argv[2];
+    phase = atoi(argv[1]);
+    player = atoi(argv[2]);
     for (i = 3; i < 34; i++)
     {
-        board[i - 3] = atoi(argv[i]);
+        scanf("%d", &board[i - 3]);
     }
 
     for(i = -15; i < 16; i++)
@@ -36,27 +49,35 @@ int main(int argc, char *argv[])
        else
           t1 -= abs(i + 1) * abs(board[i + 15]);
     }
+
+    /* Phase 1 ! */
     
     for (i = 0; i < 31; i++)
     {
-        if (board[i] > 0)
-            p1w[board[i]] = 1;
-        else if (board[i] < 0)
-            p2w[-1 * board[i]] = 1;
+        if (board[i] > 0 && i != 12)
+            p1w[board[i] - 1] = 1;   
+        if (board[i] < 0)
+            p2w[-1 * board[i] - 1] = 1; 
     }
+
+    printf("%d\n", alpha_better());
+    printf("%d\n", '-');
 
     return 0;
 } /* end of main */ 
 
-void torques(int *torque, moves)
+void torques(int *torque, char *moves)
 {
    int i, tt0 = t0, tt1 = t1, len, pos, w;
 
    len = strlen(moves);
-   for (i = 0; i < len + 1; i = i + 4)
+   for (i = 0; i < len + 1; i = i + 2)
    {
-      pos = atoi(moves[i])*10 + atoi(moves[i + 1]);
-      w = atoi(moves[i + 2]) < 0 : -1* atoi(moves[i + 3]) : atoi(moves[i + 3]);
+      pos = moves[i];
+      if (moves[i + 1] < 20)
+          w = moves[i + 1];
+      else
+          w = moves[i + 1] - 20;
       pos -= 15; /* Since board goes from 0 to 31, but positions go from -15 to 15 */
       if(pos < -3)
          tt0 += abs(pos + 3) * abs(w);
@@ -69,53 +90,54 @@ void torques(int *torque, moves)
    }
    torque[0] = tt0;
    torque[1] = tt1;
-
-   return(0);
 }
 
-int play(moves)
+int play(char *moves)
 {
    int tply = player;
-   if (strlen(moves)/4 % 2) == 1
-      tply = player > 0 : player * -1 : player;
+   if ((strlen(moves)/4 % 2) == 1)
+      if (player > 0)
+         tply = player * -1;
+      else
+         tply = player;
    return(tply);
 }
 
 
-void apply_moves(moves, *board_tmp)
+void apply_moves(char *moves, int *board_tmp)
 {
-    int len = strlen(moves), pos, i;
+    int len = strlen(moves), pos, i, w;
     for (i = 0; i < 32; i++)
     {
         board_tmp[i] = board[i];
     }
-    for (i = 0; i < len + 1; i = i + 4)
+    for(i = 0; i < len + 1; i = i + 2)
     {
-        pos = atoi(moves[i])*10 + atoi(moves[i + 1]);
-        w = moves[i + 2] == '-' : -1* atoi(moves[i + 3]) : atoi(moves[i + 3]);
+        pos = moves[i];
+        printf("%d", pos); 
+        w = (moves[i + 1] < 20) ? -1 * moves[i + 1] : moves[i + 1] - 20;
         board_tmp[pos] = w;
     }
-   return 0;
 }
 
 /* Takes in a variables number of moves, realizes them in the board
  * calculates a score
  */
-int eval_fn(moves)
+int eval_fn(char *moves)
 {
     int score = 0, board_tmp[31];
-    int board_tmp = apply_moves(moves, board_tmp);
+    apply_moves(moves, board_tmp);
     /* calculate score */
     score = 7;
     return score;
 }
 
 
-int max_value(char *moves, int alpha, int beta, int depth)
+int value(char *moves, int alpha, int beta, int depth, int max)
 {
     int v = -inf, i, tp1w[7], tp2w[7], next, board_tmp[31], j, p;
     int t[2];
-    char nmove[4];
+    char nmove[2];
     if (depth > d)
         return eval_fn(moves);
     for (i = 0; i < 7; i++)
@@ -124,12 +146,12 @@ int max_value(char *moves, int alpha, int beta, int depth)
          tp2w[i] = p2w[i];
     }
     /* */
-    for (i = 0; i < strlen(moves); i = i + 4)
+    for (i = 0; i < strlen(moves); i = i + 2)
     {
-        if (moves[i + 2] == '-')
-            tp2w[atoi(moves[i + 3])] = 0;
+        if (moves[i + 1] < 20)
+            tp2w[moves[i + 1]] = 0;
         else
-            tp1w[atoi(moves[i + 3])] = 0;
+            tp1w[moves[i + 1] - 20] = 0;
     }
 
     apply_moves(moves, board_tmp);
@@ -146,31 +168,39 @@ int max_value(char *moves, int alpha, int beta, int depth)
                {
                   if(tp1w[j])
                   {
-                     nmove[0] = (char) i / 10 + 48;
-                     nmove[1] = (char) i % 10 + 48;
-                     nmove[2] = (char) 48;
-                     nmove[3] = (char) j + 48;
+                     nmove[0] = (char) i;
+                     nmove[1] = (char) j + 1;
                   }
                }
                else
                {
                   if(tp2w[j])
                   {
-                     nmove[0] = (char) i / 10 + 48;
-                     nmove[1] = (char) i % 10 + 48;
-                     nmove[2] = '-';
-                     nmove[3] = (char) j + 48;       
+                     nmove[0] = (char) i;
+                     nmove[1] = (char) j + 21;       
                   }
                }
                torques(t, nmove);
                if(!(t[0] > 0 || t[1] < 0))
                {  
-                  next = 1;
-                  v = max(v, min_value(strcat(moves, nmove), 
-                                       alpha, beta, depth + 1));
-                  if (v >= beta)
-                      return v;
-                  alpha = max(alpha, v);
+                  if (max)
+                  {
+                      next = 1;
+                      v = max(v, value(strncat(moves, nmove, 2), 
+                                           alpha, beta, depth + 1, 0));
+                      if (v >= beta)
+                          return v;
+                      alpha = max(alpha, v);
+                  }
+                  else
+                  {
+                      next = 1;
+                      v = min(v, value(strncat(moves, nmove, 2), 
+                                           alpha, beta, depth + 1, 1));
+                      if (v <= alpha)
+                          return v;
+                      beta = min(beta, v);
+                  }
                }
             }
         }
@@ -179,78 +209,7 @@ int max_value(char *moves, int alpha, int beta, int depth)
         return player * play(moves) * inf * -1;
     return v;
 }
-
-int min_value(char *moves, int alpha, int beta, int depth)
-{
-    int i, tp1w[7], tp2w[7], next, board_tmp[31], j, p;
-    int v = inf;
-    int t[2];
-    char nmove[4];
-    if (depth > d)
-        return eval_fn(moves);
-    for (i = 0; i < 7; i++)
-    {
-         tp1w[i] = p1w[i];
-         tp2w[i] = p2w[i];
-    }
-    /* */
-    for (i = 0; i < strlen(moves); i = i + 4)
-    {
-        if (moves[i + 2] == '-')
-            tp2w[atoi(moves[i + 3])] = 0;
-        else
-            tp1w[atoi(moves[i + 3])] = 0;
-    }
-
-    apply_moves(moves, board_tmp);
-
-    p = play(moves);
-
-    for (i = 0; i < 31; i++)
-    {
-w
-        if(!board_tmp[i])
-        {
-            for(j = 0; j < 7; j++)
-            {
-               if(p > 0)
-               {
-                  if(tp1w[j])
-                  {
-                     nmove[0] = (char) i / 10 + 48;
-                     nmove[1] = (char) i % 10 + 48;
-                     nmove[2] = (char) 48;
-                     nmove[3] = (char) j + 48;
-                  }
-               }
-               else
-               {
-                  if(tp2w[j])
-                  {
-                     nmove[0] = (char) i / 10 + 48;
-                     nmove[1] = (char) i % 10 + 48;
-                     nmove[2] = '-';
-                     nmove[3] = (char) j + 48;       
-                  }
-               }
-               torques(t, nmove);
-               if(!(t[0] > 0 || t[1] < 0))
-               {  
-                  next = 1;
-                  v = min(v, max_value(strcat(moves, nmove), 
-                                       alpha, beta, depth + 1));
-                  if (v <= alpha)
-                      return v;
-                  beta = min(beta, v);
-               }
-            }
-        }
-    }
-    if (!next)
-        return player * play(moves) * inf * -1;
-    return v;
-}
-
+                                 
 /* Recrusively creates feasible sequences of nontipping moves and calls
  * the evaulation function
  */
@@ -260,7 +219,8 @@ int alpha_better()
     int i, j, p;
     int v = inf;
     int t[2];
-    char nmove[4];
+    char nmove[2];
+    char best_nmove[2];
 
     p = player;
 
@@ -274,42 +234,33 @@ int alpha_better()
                {
                   if(p1w[j])
                   {
-                     nmove[0] = (char) i / 10 + 48;
-                     nmove[1] = (char) i % 10 + 48;
-                     nmove[2] = (char) 48;
-                     nmove[3] = (char) j + 48;
+                     nmove[0] = (char) i;
+                     nmove[1] = (char) j + 1;
                   }
                }
                else
                {
                   if(p2w[j])
                   {
-                     nmove[0] = (char) i / 10 + 48;
-                     nmove[1] = (char) i % 10 + 48;
-                     nmove[2] = '-';
-                     nmove[3] = (char) j + 48;       
+                     nmove[0] = (char) i;
+                     nmove[1] = (char) j + 21;       
                   }
                }
                torques(t, nmove);
                if(!(t[0] > 0 || t[1] < 0))
                {  
-                  x = min_value(nmove, -1 * inf, inf, 0)
+                  x = value(nmove, -1 * inf, inf, 0, 0);
                   if (x < best_v)
                   {
-                      best_v = x
+                      best_v = x;
                       best_nmove[0] = nmove[0];
                       best_nmove[1] = nmove[1];
-                      best_nmove[2] = nmove[2];
-                      best_nmove[3] = nmove[3];
                   }
                }
             }
         }
     }
-    if (!next)
-        return player * play(moves) * inf * -1;
-
-    return best_v
+    return best_v;
 }
 
 
