aa920af24d5e41dae7daf32f323354642649db7d
memberofTEAM <waefz@hotmail.com>
2013-09-30 20:26:25 -0700

    awww yus

--- a3/abp.c
+++ a3/abp.c
@@ -13,8 +13,14 @@ int inf = 999999999;
 int p1w[12];
 int p2w[12];
 int offset = 40;
-
-int d = 24;
+int stable[88];
+int w1 = 0;
+int w2 = 0;
+int w3 = 3;
+int w4 = 10;
+int w5 = 7;
+int w6 = 15;
+int d = 20;
 
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
@@ -34,6 +40,7 @@ void alpha_better();
 int main(int argc, char *argv[])
 {
     int i;
+    FILE *file;
 
     player = atoi(argv[1]);
     phase = atoi(argv[2]);
@@ -42,7 +49,15 @@ int main(int argc, char *argv[])
     {
         board[i - 3] = atoi(argv[i]);
     }
-    
+
+    file = fopen("stable.dat", "r");
+    assert(file);
+   
+    for(i = 0; i < 88; i++)
+    {
+       fscanf(file, "%d", &stable[i]);
+    }
+    fclose(file);
     
     //phase = 1;
     //player = 1;
@@ -87,6 +102,24 @@ void torques(int *torque)
    torque[1] = tt1;
 }
 
+/* tests torque without updating global torque. Use for scoring */
+int ttorque(int weight, int pos, int *ttor, int *tor)
+{
+   int tt0, tt1;
+   tor[0] = ttor[0];
+   tor[1] = ttor[1];
+
+   if(pos < -3)
+      tor[0] += abs(pos + 3) * abs(board[pos + 15]);
+   else
+      tor[0] -= abs(pos + 3) * abs(board[pos + 15]);
+   if(pos < -1)
+      tor[1] += abs(pos + 1) * abs(board[pos + 15]);
+   else
+      tor[1] -= abs(pos + 1) * abs(board[pos + 15]);
+}
+
+
 int tipped(int *t)
 {
     return (t[0] > 0 || t[1] < 0);
@@ -96,19 +129,98 @@ int tipped(int *t)
  */
 int eval_fn(int exhausted)
 {
-    int score, t[2];
-    /* calculate score */
-    torques(t);
-    score = abs(t[0]*t[1]);
+    int score, i, j;
+    int tor[2], ttor[2];
+    int *pw;
+    int numstab = 0; /* current number of stable blocks */
+    int possible = 0;/* possible stable blockers */
+    int possible2 = 0;
+    int numopen = 0; /* number of open stable positions */
+    int other = 0; /* moves on right side of board that aren't stable blockers */
+    int numblock = 0;
+
+    torques(ttor);
+    score = 0;
+
+    if(player > 0) 
+       pw = p1w;
+    else
+       pw = p2w;
+
+    /* checks for moves available that aren't stable */
+    for(i = 0; i < 87; i = i + 2)
+    {
+       for(j = 0; j < 12; j++)
+       {
+          if(pw[j])
+          { 
+             if(j + 1 != stable[i])
+             {
+                ttorque(j + 1, stable[i + 1], ttor, tor);
+                if(tor[0] <= 0 && tor[1] >= 0)
+                {
+                   possible++; /* possibly do some torque multiplication here */
+                }
+             }
+          } 
+       }
+       if(possible)
+       {
+          possible2++;
+          possible = 0;
+       }
+    }
+
+
+    for(i = 0; i < 87; i = i + 2)
+    {
+       if(board[stable[i + 1] + 15] == stable[i])
+          numstab++;
+    }
+
+
+    j = 0;
+    for(i = 0; i < 87; i = i + 2)
+    {
+       if(board[stable[i + 1] + 15] != stable[i] && board[stable[i + 1] + 15] != 0 && j != stable[i + 1])
+       {
+          numblock++;
+          j = stable[i + 1];
+       }
+    }
+
+    /* other moves that aren't stable removing moves */
+    for(i = -1; i < 16; i++)
+    {
+       for(j = 0; j < 12; j++)
+       {
+          if(p1w[j] != 0)
+          {
+             ttorque(j + 1, i, ttor, tor);
+             if(tor[0] <= 0 && tor[1] >= 0)
+                other++;  /* possibly some torque multiplication here */
+          }
+       }
+    }
+
+    for(i = 0; i < 15; i++)
+    {
+       if(board[i] == 0)
+          numopen++;
+    }
+
+    score = w1 * possible2 + w2 * other + w3 * (-1 * ttor[0]) - w4 * numstab - w5*numopen + w6 * numblock;
+   
     if (exhausted)
     {
         player = -1 * player;
         return score;
-//        return player * inf;
     }
     else
+    {
         player = -1 * player;
         return score;
+    }
 }
 
 int value(int alpha, int beta, int depth, int max)
