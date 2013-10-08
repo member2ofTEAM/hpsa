d98f9d94d7f73df23769dbeabb374d6468b0bb6f
member2ofTEAM <cp1995@nyu.edu>
2013-10-08 12:23:15 -0700

    wdw

--- a4/TEAM_final.py
+++ a4/TEAM_final.py
@@ -10,16 +10,16 @@ from subprocess import Popen, PIPE
 from sys import argv
 import socket
 import pdb
+from time import time
 
 teamName = "TEAM"
 eom = "<EOM>"
 port = 5555
 maxlen = 999999
+TIME_CUTOFF = 100
 
 if argv[1]:
   port = int(argv[1])
-
-
     
 def distance(x, y):
     return abs(x[0] - y[0]) + abs(x[1] - y[1])
@@ -68,6 +68,13 @@ class Patient():
         self.time = time
         self.id = id
 
+def _sort_wrt(list1, list2):
+    for i in range(len(list1)):
+        l = map(lambda x: distance(x, list1[i]), list2[i:])
+        j = l.index(min(l)) + i
+        (list2[i], list2[j]) = (list2[j], list2[i])
+
+start_time = time()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', port))
 all_patients = []
@@ -86,10 +93,15 @@ hosp_ambulances = map(int, lines[304:309])
 hosp_ambulances = zip(hosp_ambulances, range(5))
 hosp_ambulances.sort(key = lambda x: int(x[0]))
 
-very_urgent = array([patient.position for patient in all_patients if patient.time<50])
+very_urgent = array([patient.position for patient in all_patients if patient.time<90])
 urgent      = array([patient.position for patient in all_patients if patient.time<120])
 bulk        = array([patient.position for patient in all_patients if patient.time>120 and patient.time<170])
 all         = array([patient.position for patient in all_patients])
+
+mean_weights = [[4, 2, 1, 0],
+                [2, 4, 4, 1],
+                [1, 2, 6, 2],
+                [0, 1, 2, 5]]
 
 total_score = 0
 best_j = 0
@@ -97,34 +109,40 @@ best_time = 0
 best_result = ""
 best_saves = 0
 for trash in range(10):
-    all_means = list(kmeans(array(data), 5, 100)[0])
-    pdb.set_trace()
-    all_means.sort(key=lambda x: 100 * distance(x, all_means[0]) * ((x[0] > x[1])*100 + 1))
-    very_urgent_means = list(kmeans(array(data), 5, 100)[0]).sort(key=lambda x: 100 * distance(x, all_means[0]) 
-        									    * ((x[0] > x[1])*100 + 1))
-    urgent_means = list(kmeans(array(data), 5, 100)[0]).sort(key=lambda x: 100 * distance(x, all_means[0]) 
-          								       * ((x[0] > x[1])*100 + 1))
-    bulk_means = list(kmeans(array(data), 5, 100)[0]).sort(key=lambda x: 100 * distance(x, all_means[0]) 
-     									     * ((x[0] > x[1])*100 + 1))
+    if (time() - start_time > TIME_CUTOFF):
+        break;
+    all_means = list(kmeans(all, 5, 100)[0])
+    very_urgent_means = list(kmeans(very_urgent, 5, 100)[0])
+    urgent_means = list(kmeans(urgent, 5, 100)[0])
+    bulk_means = list(kmeans(bulk, 5, 100)[0])
+    _sort_wrt(all_means, very_urgent_means)
+    _sort_wrt(all_means, urgent_means)
+    _sort_wrt(all_means, bulk_means)
     means = 5 * [0]
     print trash
-    for meansscale in range(100, 101, 5):
-        meansscale = float(meansscale) / 100.0;
+    for weight_config in range(4):
         for timefactor in range(1):
+            if (time() - start_time > TIME_CUTOFF):
+                break;
             times = []
             patients = all_patients
             ambulances = []
             cluster_distances = []
-                      
+
+            mw = mean_weights[weight_config]
             for i in range(len(means)):
-                means[i] = (((urgent_means[j][0] - all_means[i][0]) * meansscale)+ all_means[i][0], 
-                            ((urgent_means[j][1] - all_means[i][1]) * meansscale)+ all_means[i][1])
-          
+                means[i] = ((very_urgent_means[i][0] * mw[0] + 
+                                  urgent_means[i][0] * mw[1] + 
+                                    bulk_means[i][0] * mw[2] + 
+                                     all_means[i][0] * mw[3])/sum(mw),
+                            (very_urgent_means[i][1] * mw[0] + 
+                                  urgent_means[i][1] * mw[1] + 
+                                    bulk_means[i][1] * mw[2] + 
+                                     all_means[i][1] * mw[3])/sum(mw))
+                means[i] = (int(round(means[i][0])), int(round(means[i][1])))
+
             #THIS IS NOT FACTORING URGENCY AT ALL
             #TODO WE HAVE TO MODIFY THE WAY WE ASSIGN AMBULANCES  
-            # THE MORE THE ANT SORES PHEROMONES THE FASTER IT CONVERGES?
-            # SOMONE HAS TO CHECK THAT IM TAKING AWAY A LINEAR AMOUNT OF AMOUNTS PER HOSPITAL
-            # HOW DO I ASSING POPEN PROCESS TO SPECIFIC CPU
 	    for mean in means:
                 cluster_distance = []
                 for patient in patients:
@@ -134,7 +152,6 @@ for trash in range(10):
             cluster_distances.sort(key=lambda x: int(x[0]))
             
             hospitals = []
-            
             def sort_wrt((p1, p2)):
                 patients.sort(key=lambda x: distance(x.position, [p1, p2]))
                 
@@ -159,7 +176,7 @@ for trash in range(10):
             f.close()
  
             total_saves = 0
-            l = [Popen(["taskset"] + ["-c " + str(x)] + ["./TEAM"], stdout = PIPE) for x in range(8)]
+            l = [Popen(["taskset"] + ["-c"] + [str(x)] + ["./TEAM"], stdout = PIPE) for x in range(8)]
             outputs = map(lambda x: x.communicate()[0].split("\n"), l)
             for output in outputs:
                 ambulances = output[:-1]
@@ -180,10 +197,15 @@ for trash in range(10):
 
                 print output[-1]
 
+                if (time() - start_time > TIME_CUTOFF):
+                    break;
+ 
+
 #TODO REMOVE AMBULANCE RESTRICTION IN C FILE
 
 #TODO IMPLEMENT THE MEANS STUFF!
-    
+   
+print best_result 
 sendResult(s, best_result)
 reply = getData(s)
 print reply
