878bc3283b2bfbab5374cc341577af1171199bf6
member2ofTEAM <cp1995@nyu.edu>
2013-10-15 16:16:45 -0700

    dwdw

--- a5/game.c
+++ a5/game.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include "tinymt32.h"
+#include <stdlib.h>
+#include <math.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "tinymt32.c"
+#include <time.h>
 
-int BOARD_SIZE = 1000;
-double board[BOARD_SIZE][BOARD_SIZE];
+void test_algorithm();
+int do_move_random(int *move);
+void print_board();
+
+const int BOARD_SIZE = 1000;
+const double INF = 10000;
+double board[1000][1000];
 // 1 is us, 0 is the other guy
 // We assume, that we always start!
 int next_to_set = 1;
@@ -20,44 +32,163 @@ void init_board()
     }
 }
 
-//move is in the format [x, y] as a 2 dim array
-void do_move(int *move)
+double distance_squared(int x0, int y0, int x1, int y1)
 {
-    
+    return (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1);
 }
 
-void undo_move(int *move)
+
+//move is in the format [x, y] as a 2 dim array
+//returns the area owned by us
+int do_move(int *move)
 {
+    int our_area, i, j, d;
+    if (move[0] < 0 || move[0] > 999 || move[1] < 0 || move[1] > 999)
+    {
+        return -1;
+    }
+    //Make sure stone can be placed
+    if (abs(board[move[0]][move[1]]) == INF)
+        return -1;
+
+    //Place the stone
+    if (next_to_set)
+        board[move[0]][move[1]] = INF;
+    else
+        board[move[0]][move[1]] = -INF;
+
+    //Update the board
+    for (i = 0; i < BOARD_SIZE; i++)
+    {
+        for (j = 0; j < BOARD_SIZE; j++)
+        {
+            //Ignore pixels with stones set
+            if (abs(board[i][j]) == INF || i == j)
+                continue;
+            d = distance_squared(move[0], move[1], i, j);
+            //If we are not next_to_set subtract the pull
+            if (!next_to_set)
+                d = -d;
+            board[i][j] += 1 / d;
+            //Our area only have positive pull
+            if (board[i][j] > 0)
+                our_area++;
+        }
+     }
+     //Flip the player
+     next_to_set = !next_to_set;
+     return our_area;
+}
+
+void print_board()
+{
+    int i, j;
+    for (i = 0; i < BOARD_SIZE; i++)
+    {
+        for (j = 0; j < BOARD_SIZE; j++)
+        {
+            printf("%17.16lf ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+//move is in the format [x, y] as a 2 dim array
+int undo_move(int *move)
+{
+    int our_area, i, j, d;
+    if (move[0] < 0 || move[0] > 999 || move[1] < 0 || move[1] > 999)
+    {
+        return -1;
+    }
+    //Make sure stone can be removed
+    if (abs(board[move[0]][move[1]]) != INF)
+        return -1;
+
+    //Remove the stone
+    board[move[0]][move[1]] = 0;
+
+    //Update the board
+    for (i = 0; i < BOARD_SIZE; i++)
+    {
+        for (j = 0; j < BOARD_SIZE; j++)
+        {
+            if (i == j)
+                continue;
+            d = distance_squared(move[0], move[1], i, j);
+            if (!next_to_set)
+                d = -d;
+            //If the considered pixel is a placed stone, add the value to 
+            //neutral pixel as appropiate
+            if (abs(board[i][j]) == INF)
+            {
+                if(board[i][j] > 0)
+                    board[move[0]][move[1]] += 1/d;
+            }
+            else
+            {
+                board[i][j] += 1 / d;
+            }
+            //Our area only has positive pull
+            if (board[i][j] > 0)
+                our_area++;
+        }
+     }
+     //Flip the player
+     next_to_set = !next_to_set;
+     return our_area;
+}
+
+void test_algorithm()
+{
+    int i, score;
+    for (i = 0; i < 20; i++)
+    {
+        int move[2];
+        do_move_random(move);
+        //printf("us: %d %d; ", move[0], move[1]);
+        print_board();
+        score = do_move_random(move);
+        //printf("them: %d %d; ", move[0], move[1]);
+        print_board();
+        //Log results etc. - maybe use csv for sorting actions later
+        //Only log if we lose?
+    }
+    printf("Final score: %d\n", score);
 
 }
 
 int main()
 {
-    init_board();
+    //init_board();
     test_algorithm();
     return 0;
 }
 
-void test_algorithm()
+void do_move_algorithm(int *move)
 {
     int i;
-    for (i = 0; i < 20; i++)
+    i = i + 1;
+}
+
+//Between 0 and 999 inclusive
+int get_random_int()
+{
+    return (int) (rand() % 1000);
+}
+
+int do_move_random(int *move)
+{
+    move[0] = get_random_int();
+    move[1] = get_random_int();
+    int score;
+    while((score = do_move(move)) < 0)
     {
-        do_move(get_move_algorithm());
-        do_move(get_move_random());
-        //Log results etc. - maybe use csv for sorting actions later
-        //Only log if we lose?
+        move[0] = get_random_int();
+        move[1] = get_random_int();
     }
-}
-
-int *get_move_algorithm()
-{
-
-}
-
-int *get_move_random()
-{
-
+    return score;
 }
 
 
