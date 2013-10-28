17182386683deef6e1202a95ab2d9ac2d58a3cf7
SvenAG <sven.giesselbach@gmail.com>
2013-10-27 23:26:07 -0400

    ++

--- a6/Evasion.py
+++ a6/Evasion.py
@@ -5,6 +5,7 @@ import time
 import random
 import msvcrt as m
 import math
+import pdb
 from sets import Set
 
 
@@ -25,7 +26,7 @@ heatmap = [[100 for value in range(500)] for value in range(500)]
 hunter_predict_x = 0
 hunter_predict_y = 0
 
-predict_steps = 30
+predict_steps = 200
 
 last_hunter_x = 0
 last_hunter_y = 0
@@ -50,13 +51,13 @@ wall_horizontal = {'-2':y_min, '-1':y_max}
 prey_moves = {(0,1):'NN',(1,1):'NE',(1,0):'EE',(1,-1):'SE',(0,-1):'SS',(-1,-1):'SW',(-1,0):'WW',(-1,1):'NW',(0,0):'XX'}
 hunter_moves = prey_moves.values() + [move+'h' for move in prey_moves.values()] + [move+'v' for move in prey_moves.values()] + ['r'+value for value in wall_horizontal.keys()+wall_vertical.keys() if int(value)>=0]
 
-prey_queue = []
+prey_queue = [(0,0)]
 prey_move = (0,0)
 
 h_x = x0
 h_y = y0
 
-x_prey = 330
+x_prey = 320
 y_prey = 200
 
 can_set = 1
@@ -85,7 +86,8 @@ def horizontal_check(pos_y):
     if pos_y >= y_max or pos_y <= y_min or pos_y in wall_horizontal.values():
         return True
         
-    
+def l1d((x, y), mid_x, mid_y):
+    return max(abs(mid_x - x), abs(mid_y - y))    
     
 def calculate_heat(steps,hunter_vx,hunter_vy,hunter_x,hunter_y,heatmap):   
     pot_x = hunter_x
@@ -93,6 +95,7 @@ def calculate_heat(steps,hunter_vx,hunter_vy,hunter_x,hunter_y,heatmap):
     
     pot_vx = hunter_vx
     pot_vy = hunter_vy
+        
     
     pos = ((x_prey,y_prey),0)
     pos_set = [pos]
@@ -100,6 +103,8 @@ def calculate_heat(steps,hunter_vx,hunter_vy,hunter_x,hunter_y,heatmap):
     visited.append(pos[0])
     heatmap[pos[0][0]][pos[0][1]] = pos[1]
     temp_moves = [move for move in prey_moves.keys() if not move == (0,0)]
+        
+    # Distance in which Prey can move within 30 steps                    
     while pos_set:
         pos = pos_set.pop(0)
                 
@@ -113,19 +118,47 @@ def calculate_heat(steps,hunter_vx,hunter_vy,hunter_x,hunter_y,heatmap):
                         visited.append((mx,my))
                         heatmap[mx][my] = pos[1]+2
                         
-    canvas.delete('predict')
-    minx = 500
-    miny = 500
-    maxx = 0
-    maxy = 0
+    
+    # Gravity
+    mid_x = 0
+    mid_y = 0
+    
+    visited = []
+    
+    h_walls = wall_horizontal.values()
+    h_walls.sort()
+    for (y_i, y_j) in zip(h_walls, h_walls[1:]):
+        if y_i < y_prey and y_prey < y_j:
+            v_walls = wall_vertical.values()
+            v_walls.sort()
+            for (x_i, x_j) in zip(v_walls, v_walls[1:]):
+                if x_i < x_prey and x_prey < x_j:
+                    mid_x = x_i+(x_j-x_i)/2
+                    mid_y = y_i+(y_j-y_i)/2
+    
+    
+                                        
+    init = ((mid_x,mid_y),0)
+        
+    l = range(500)
+    
+    for i in range(500):
+        for j in range(500):
+            heatmap[i][j] = heatmap[i][j] + 1000*(l1d((i, j), mid_x, mid_y))
+          
+    # Prediction of Hunter Moves            
     for i in range(steps):        
-        for k in range(8):
-            for l in range(8):
+        canvas.delete('predict'+str(i))
+    for i in range(steps): 
+        print i
+        minx = 500
+        miny = 500
+        maxx = 0
+        maxy = 0       
+        for k in range(10):
+            for l in range(10):
                 offset_x = pot_x+k-4
-                offset_y = pot_y+l-4
-                
-                print offset_x
-                print offset_y
+                offset_y = pot_y+l-4                
                 
                 if not(vertical_check(offset_x)):
                     if not(vertical_check(offset_y)):
@@ -139,6 +172,8 @@ def calculate_heat(steps,hunter_vx,hunter_vy,hunter_x,hunter_y,heatmap):
                         if offset_y < miny:
                             miny = offset_y
     
+        canvas.create_rectangle(minx, miny, maxx, maxy, fill = "yellow", tag = 'predict'+str(i))
+        canvas.update() 
         pot_x = pot_x + pot_vx
         pot_y = pot_y + pot_vy
         # If a boundary has been crossed, reverse the direction
@@ -147,9 +182,6 @@ def calculate_heat(steps,hunter_vx,hunter_vy,hunter_x,hunter_y,heatmap):
     
         if horizontal_check(pot_y):
             pot_vy = pot_vy*-1 
-            
-    canvas.create_rectangle(minx, miny, maxx, maxy, fill = "yellow", tag = 'predict')
-    canvas.update() 
             
     
 def euclidean_distance(pos1,pos2):
@@ -177,7 +209,7 @@ def find_safest_path(pos):
             if not (mx,my) in checked:
                 if not(vertical_check(mx)):
                     if not(horizontal_check(my)):
-                        temp_queue.append((heatmap[mx][my]+1000*euclidean_distance((mx,my),(x_prey,y_prey)),(move[0],move[1])))
+                        temp_queue.append((heatmap[mx][my]+100*euclidean_distance((mx,my),(x_prey,y_prey)),(move[0],move[1])))
                         checked.append((mx,my))
         temp_queue.sort()
         print temp_queue[0]
@@ -259,14 +291,18 @@ while(1):
                             set_count = 0
              
                   
-    if tick % 2 and tick > 0:
+    if tick % 2 and tick > 1:
         
-        if len(prey_queue)==0:
+        if len(prey_queue)==1:
         
-            hunter_predict_x = h_x - last_hunter_x
-            hunter_predict_y = h_y - last_hunter_y
-            if(hunter_predict_x != last_hunter_x-h_x) or (hunter_predict_y != last_hunter_y-h_y):                                
+            last_predict_x = hunter_predict_x
+            last_predict_y = hunter_predict_y
+            hunter_predict_x = vx
+            hunter_predict_y = vy
+            if(hunter_predict_x != last_predict_x) or (hunter_predict_y != last_predict_y):
+                                                
                 heatmap = [[100 for value in range(500)] for value in range(500)] 
+                
                 calculate_heat(predict_steps,hunter_predict_x,hunter_predict_y,h_x,h_y,heatmap)
                                 
                 thresh = []
@@ -287,10 +323,17 @@ while(1):
                                                 value = (heatmap[x_offset][y_offset],(x_offset,y_offset))
                                                 thresh.append(value)
                             
-                thresh.sort()   
+                thresh.sort()  
+                #pdb.set_trace()
+                last_hunter_x = h_x
+                last_hunter_y = h_y 
                 
                 pos = thresh[0][1]
                 print pos
+                
+                canvas.delete('prey_pos')
+                canvas.create_rectangle(pos[0]-5, pos[1]-5, pos[0]+5, pos[1]+5, fill = "green", tag = 'prey_pos')
+                canvas.update() 
                 
                 prey_queue = find_safest_path(pos)
                 print prey_queue
