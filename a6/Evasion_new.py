21eb856bb6d91d2dd69e9405680e66bb8669d93d
member2ofTEAM <cp1995@nyu.edu>
2013-10-28 22:25:21 -0700

    wd

--- a6/Evasion_new.py
+++ a6/Evasion_new.py
@@ -28,12 +28,10 @@ vx = 1    # x velocity
 vy = 1    # y velocity
 
 # Boundaries
-x_min = 0
-y_min = 0
-x_max = 499
-y_max = 499
-
-MAX_HEAT = 1000000
+x_min = -1
+y_min = -1
+x_max = 500
+y_max = 500
 
 wall_vertical_out = [(-4,(x_min,y_min),(x_min,y_max)),(-3,(x_max,y_min),(x_max,y_max))]
 wall_horizontal_out = [(-2,(x_min,y_min),(x_max,y_min)),(-1,(x_min,y_max),(x_max,y_max))]
@@ -65,34 +63,27 @@ pause = -1
 ticks_to_set = 30
 max_walls = 15
             
-heatmap = [[0 for value in range(500)] for value in range(500)] 
-
 id = 0
-
-PREY_QSIZE = 30
 
 canvas.create_rectangle(x_prey, y_prey, x_prey + 4, y_prey + 4, fill = "blue", tag = 'prey')
 canvas.create_rectangle(h_x, h_y, h_x+4, h_y+4, fill="red", tag='blueball')
     
 def vertical_check(pos_x):
-    return pos_x >= x_max or pos_x <= x_min or pos_x in wall_vertical.values()
+    return pos_x in wall_vertical.values()
     
 def horizontal_check(pos_y):
-    return pos_y >= y_max or pos_y <= y_min or pos_y in wall_horizontal.values()
-        
-def lmd((x, y), mid_x, mid_y):
-    return max(abs(mid_x - x), abs(mid_y - y))   
+    return pos_y in wall_horizontal.values()
     
 #Returns bounds on x and y
 def prey_update_box():
     h_walls = wall_horizontal.values()
     h_walls.sort()
     for (y_i, y_j) in zip(h_walls, h_walls[1:]):
-        if y_i <= y_prey and y_prey < y_j:
+        if y_i < y_prey and y_prey < y_j:
             v_walls = wall_vertical.values()
             v_walls.sort()
             for (x_i, x_j) in zip(v_walls, v_walls[1:]):
-                if x_i <= x_prey and x_prey < x_j:
+                if x_i < x_prey and x_prey < x_j:
                     return ((x_i, x_j), (y_i, y_j))
 
 prey_boundary = prey_update_box()
@@ -124,60 +115,59 @@ def prey_pos_in_box(pos):
             prey_boundary[1][0] < pos[1] and
             prey_boundary[1][1] > pos[1])
 
-def calculate_heat(steps,hunter_vx,hunter_vy,hunter_x,hunter_y,heatmap):   
-    pot_x = hunter_x
-    pot_y = hunter_y
-    pot_vx = hunter_vx
-    pot_vy = hunter_vy
-        
-    # Distance in which Prey can move within 30 steps
-    for i in range(-15 + x_prey, 16 + x_prey):
-        for j in range(-15 + y_prey, 16 + y_prey):
-             if prey_pos_in_box((i, j)):
-                 heatmap[i][j] = lmd((i, j), x_prey, y_prey)
-    
+img = PhotoImage(width=500, height=500)
+canvas.create_image((250, 250), image=img, state="normal")
+PREY_QSIZE = 100
+HUNTER_PSIZE = 2 * PREY_QSIZE
+MAX_HEAT = 708
+
+def distance(pos1,pos2):
+    return math.sqrt(abs(pos1[0]-pos2[0])**2 + abs(pos1[1]-pos2[1])**2)
+ 
+def calculate_heat(steps, pot_vx, pot_vy, pot_x, pot_y, heatmap):
+    # Prediction of Hunter Moves            
+    hunter_steps = []
+    for i in range(HUNTER_PSIZE):
+        hunter_steps.append((pot_x, pot_y))
+        if vertical_check(pot_x + pot_vx):
+            pot_vx = pot_vx*-1
+        if horizontal_check(pot_y + pot_vy):
+            pot_vy = pot_vy*-1
+        if vertical_check(h_x + vx):
+            pot_vx = 0
+        if horizontal_check(h_y + vy):
+            pot_vy = 0
+        (pot_x, pot_y) = (pot_x + pot_vx, pot_y + pot_vy)
+
     # Gravity
     ((x_i, x_j), (y_i, y_j)) = prey_boundary 
     mid_x = x_i+(x_j-x_i)/2
     mid_y = y_i+(y_j-y_i)/2
-    
-    for i in range(500):
-        for j in range(500):
-            heatmap[i][j] = heatmap[i][j] + 1000*(lmd((i, j), mid_x, mid_y))
-          
-    # Prediction of Hunter Moves            
-    for i in range(steps): 
-        canvas.delete('predict'+str(i))
-        for k in range(pot_x - 5, pot_x + 5):
-            for l in range(pot_y - 5, pot_y + 5):
-                if hunter_pos_in_box((k, l)):
-                    heatmap[k][l] += 50 + steps-(int(i)/2)
-    
-        canvas.create_rectangle(pot_x, pot_y, pot_x + 4, pot_y + 4, fill = "yellow", tag = 'predict'+str(i))
-        canvas.update() 
-        pot_x = pot_x + pot_vx
-        pot_y = pot_y + pot_vy
-        # If a boundary has been crossed, reverse the direction
-        if vertical_check(pot_x):
-            pot_vx = pot_vx*-1
-    
-        if horizontal_check(pot_y):
-            pot_vy = pot_vy*-1 
-            
-    
-def euclidean_distance(pos1,pos2):
-    return math.sqrt((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2)   
-    
+    for i in range(x_prey - 100, x_prey + 100):
+        for j in range(y_prey - 100, y_prey + 100):
+            heatmap[i][j] = distance((i, j), (mid_x, mid_y))
+
+    for (x, y) in hunter_steps:
+        for i in range(x - 10, x + 10):
+            for j in range(y - 10, y + 10):
+                if distance((i, j), (x, y)) < 10 and hunter_pos_in_box((i, j)):
+                    heatmap[i][j] = max(MAX_HEAT - distance((x, y), (i, j)), heatmap[i][j])
+
+    for i in range(x_prey - 100, x_prey + 100):
+        for j in range(y_prey - 100, y_prey + 100):
+            img.put(str('#%02x%02x%02x' % ((255 * int(heatmap[i][j]))/MAX_HEAT, 0, 0)), (i,j))
+    canvas.update()  
+
+heatmap = [[0 for value in range(500)] for value in range(500)] 
+calculate_heat(predict_steps,vx,vy,h_x,h_y,heatmap)
+   
 #Return coordinates of minimum entry in heatmap
 def find_safest_path(prey_queue):
-    print "current_pos is" + str((x_prey,y_prey))
-    del  prey_queue[:]
     start = (x_prey, y_prey)
-    temp_moves = [move for move in prey_moves.keys() if not move == (0,0)]
-    #pdb.set_trace()
     for trash in range(PREY_QSIZE):
-        best_next = [MAX_HEAT, (-1, -1)]
-        for move in temp_moves:
+        #By default the prey doesn't move
+        best_next = [MAX_HEAT, (0, 0)]
+        for move in prey_moves.keys():
             maybe_next = (start[0] + move[0], start[1] + move[1])
             if prey_pos_in_box(maybe_next):
                (mx, my) = maybe_next
@@ -186,9 +176,6 @@ def find_safest_path(prey_queue):
                    best_next[0] = score
                    best_next[1] = move
 
-        #The prey may be cornered and can't move anymore
-        if best_next[1] == (-1, -1):
-            break
         move = best_next[1]
         start = (start[0] + move[0], start[1] + move[1])
         prey_queue.append(move)
@@ -205,11 +192,10 @@ window.bind('<Key>', handler)
 tick = 0
 # For each timestep
 while(1):
-   
     #Build wall 
     if can_set and len(wall_horizontal.keys()+wall_vertical.keys())<max_walls:
         #build vertical wall
-        if abs(x_prey-h_x) <= 4 and abs(h_x-x_prey) > 0 and prey_pos_in_box((h_x, h_y)):
+        if prey_pos_in_box((h_x, h_y)) and h_x + vx == x_prey and h_x != x_prey:
            id = max(map(lambda x: int(x), wall_vertical.keys() + wall_horizontal.keys())) + 1
            wall_vertical[str(id)] = h_x
            prey_boundary = prey_update_box()
@@ -220,11 +206,10 @@ while(1):
            canvas.update()
            can_set = 0
            set_count = 0                           
-           calculate_heat(predict_steps,hunter_predict_x,hunter_predict_y,h_x,h_y,heatmap)
            del prey_queue[:]
                             
         #build horizontal wall
-        if abs(y_prey - h_y)<=4 and abs(y_prey - h_y) > 0 and prey_pos_in_box((h_x, h_y)):
+        if prey_pos_in_box((h_x, h_y)) and h_y + vy == y_prey and h_y != y_prey:
            id = max(map(lambda x: int(x), wall_vertical.keys() + wall_horizontal.keys())) + 1
            wall_horizontal[str(id)] = h_y
            prey_boundary = prey_update_box()
@@ -235,19 +220,17 @@ while(1):
            canvas.update()
            can_set = 0
            set_count = 0
-           calculate_heat(predict_steps,hunter_predict_x,hunter_predict_y,h_x,h_y,heatmap)
            del prey_queue[:]
              
     #Update prey   
     if tick % 2 and tick > 1:
         if not prey_queue: 
+            calculate_heat(predict_steps,vx,vy,h_x,h_y,heatmap)
             find_safest_path(prey_queue)
-            print prey_queue
         
         #There may be nothing more to do
         if prey_queue:
             prey_move = prey_queue.pop(0)
-            print prey_queue
             x_prey = x_prey + prey_move[0]
             y_prey = y_prey + prey_move[1]
         
@@ -260,6 +243,10 @@ while(1):
         vx = vx*-1
     if horizontal_check(h_y + vy):
         vy = vy*-1
+    if vertical_check(h_x + vx):
+        vx = 0
+    if horizontal_check(h_y + vy):
+        vy = 0
     h_x = h_x + vx
     h_y = h_y + vy
     hunter_boundary = hunter_update_box()
