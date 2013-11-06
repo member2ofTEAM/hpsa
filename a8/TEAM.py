568ebf378f08f459bb7aea0cf7b481e2475073d5
member2ofTEAM <cp1995@nyu.edu>
2013-11-06 14:44:25 -0800

    wd

--- a8/TEAM.py
+++ a8/TEAM.py
@@ -6,9 +6,11 @@ import time
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+
 def send(msg):
     print "sending"
     print "Send: " + msg
+    msg += "<EOM>\n"
     totalsent = 0
     while totalsent < len(msg):
         sent = s.send(msg[totalsent:])
@@ -18,7 +20,7 @@ def send(msg):
 
 def receive():
     msg = ''
-    while (1):
+    while '<EOM>\n' not in msg:
         chunk = s.recv(1024)
         if not chunk: break
         if chunk == '':
@@ -32,9 +34,9 @@ def parse_data(data):
     init = data[0]
     rest = data[1:]
     n = int(init.split(" ")[1])
-    pre_data = n*[0]
-    for i in range(n):
-        pre_data[i] = map(int, rest[i].split(" "))
+    pre_data = 20*[0]
+    for i in range(20):
+        pre_data[i] = map(float, rest[i].split(" ")[:-1])
     return (n, np.array(pre_data))
 
 def parse_update(data):
@@ -48,12 +50,13 @@ def i_zeros(n, i):
     last = n % i
     rest = n / i
     result = []
-    for j in range(firsts):
+    for j in range(i):
         if j % 2:
             result.append([1] * rest)
         else:
             result.append([0] * rest)
-    result.append(last * [1])
+    if last:
+        result.append(last * [1])
     #flatten the result
     return [item for sublist in result for item in sublist]
 
@@ -61,12 +64,25 @@ def i_zeros(n, i):
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('127.0.0.1', int(sys.argv[1])))
-    send("TEAM")
+    receive()
     pdb.set_trace()
+    send("TEAM")
     (n, init_data) = parse_data(receive())
     clf = LinearRegression()
-    for i in range(20):
-        clf.fit(init_data[:,:-1], init_data[:,-1])
-        print clf.coef_
-        init_data.append(parse_update(receive(), axis=1)
-    
+
+    for i in range(19):
+        clf.fit(init_data[:, :-1], init_data[:,-1])
+        w = clf.coef_
+        zeros = ""
+        for zero in i_zeros(n, i + 2):
+            zeros += str(zero) + " "
+        zeros = zeros[:-1]
+        send(zeros)
+        update = parse_update(receive())
+        init_data.append(update, axis=1)
+
+    candidate = n * [0]
+    for i in range(len(w)):
+        if w[i] > 0:
+            candidate[i] = 1
+
