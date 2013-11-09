4225d77a13c06b5df4dc5bbd2398e2dacf5aca75
member3ofTEAM <sag545@nyu.edu>
2013-11-08 16:24:54 -0800

    Fixes and Beautifulness

--- a9/file_reader.py
+++ a9/file_reader.py
@@ -1,8 +1,22 @@
-import sys
+import argparse
 
-item_list = open(sys.argv[1])
-item_list = item_list.read()
-item_list.split(" ")
+#Create and Parse arguments
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('itemlist', metavar='list',
+                   help='Filename of the Itemlist')
+args = parser.parse_args()
+
+#Open file specified from arguments
+#And read in files
+input = open(args.itemlist)
+item_list = input.read()
+input.close()
+
+#Create list of items as integers
+item_list = item_list.split(" ")
+list = []
 
 for item in item_list:
-  print item," ",
+  list.append(int(item))
+
+print list
--- a9/item_gen.py
+++ a9/item_gen.py
@@ -1,6 +1,7 @@
 import random
 import argparse
 
+#Parse Arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('nu_diff', metavar='N', type=int,
                    help='The number of different items')
@@ -9,18 +10,26 @@ parser.add_argument('list_len', metavar='A', type=int,
 
 args = parser.parse_args()
 
+#Open File to write in
 item_list = open('itemlist','wb')
 rand_list = []
 
+#Create list of numbers
 #TODO: Use better seed
 random.WichmannHill(10)
 
 for i in range(args.list_len):    
     rand_list.append(random.randint(0,args.nu_diff))
-    
+
+#Shuffle one more time for extra randomness!
 random.shuffle(rand_list)
+
+#Write to file
+entry = ''
   
 for i in rand_list:
-  item_list.write(str(i)+" ")
+  entry += str(i)+" "
+  
+item_list.write(entry[:-1])
   
 item_list.close()
