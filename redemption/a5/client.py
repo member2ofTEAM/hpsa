dd6b8c3c05660cb711c905ca40d3c640820c13a9
member2ofTEAM <cp1995@nyu.edu>
2013-11-29 16:56:17 -0800

    wd

--- redemption/a5/client.py
+++ redemption/a5/client.py
@@ -1,24 +1,19 @@
-import pdb
 import socket, sys
+import random
+import pdb
 import time
-import itertools as it
+import numpy as np
 from exceptions import ZeroDivisionError
-from subprocess import Popen, PIPE
 
-teamname="TEAM"
+teamname="RandomBrie"
 port=4567
 eom = "<EOM>"
 maxlen = 999999
 dim=1000
-automatic=1
 print(sys.argv)
 if len(sys.argv)>1:
     port = int(sys.argv[1])
-if len(sys.argv)>2:
-    teamname = str(sys.argv[2])
-if len(sys.argv)>3:
-    automatic = int(sys.argv[3])
-  
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', port))
 
@@ -33,109 +28,94 @@ def readsocket(sock,timeout=0):
         if eom in inpData:
             break
     inpData=inpData.strip()[:-len(eom)]
-#    serversaid(inpData.replace('\n', ' [N] ')[:90])
+    serversaid(inpData.replace('\n', ' [N] ')[:90])
     return inpData.strip()
 
 def sendsocket(sock,msg):
     msg += eom
     totalsent=0
-    MSGLEN = len(msg) 
+    MSGLEN = len(msg)
     while totalsent < MSGLEN:
         sent = sock.send(msg[totalsent:])
         if sent == 0:
             raise RuntimeError("socket connection broken")
         totalsent = totalsent + sent
     isaid(msg)
-    
+
+def makemove(socket,pid,x,y):
+    sendsocket(socket,"(%d,%d,%d)"%(pid,x,y))
+
 def serversaid(msg):
     print("Server: %s"%msg[:80])
 def isaid(msg):
     print("Client: %s"%msg[:80])
-def makemove(socket,pid,x,y):
-    sendsocket(socket,"(%d,%d,%d)"%(pid,x,y))
-def distance_squared(x0, y0, x1, y1):
-    return (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1)
-
 class State:
-    def __init__(self):
+    def __init__(self,noplayers,Nstones,playerid):
         # personal
-        self.moves = []
-        self.somezip = lambda *x: it.islice(it.izip_longest(*x), len(x[0]))
+        self.playerid=playerid
+        # game parameters
+        self.Nstones=Nstones
+        self.noplayers=noplayers
+        # changing game
+        self.nextplayer=0
+        self.timeleft=-1.00
+        self.moves=[]
 
-    def update_state(self, statestr):
-        statestr = statestr.split("\n")
-        if statestr[1]:
-            exec("self.moves = [" + statestr[1] + "]")
-            p1moves = []
-            p2moves = []
-            for move in self.moves:
-                if move[0] == 1:
-                    p1moves.append(move[1:])
-                else:
-                    p2moves.append(move[1:])
-            self.moves = self.somezip(p1moves, p2moves)
-            self.moves = [item for sublist in self.moves for item in sublist]
+    def parsestate(self,statestr):
+        state=statestr.split('\n')
+        line1=state[0].split(',')
+        self.nextplayer=int(line1[0])
+        if self.nextplayer==self.playerid:
+            self.timeleft=float(line1[1])
+        self.parsemoves(state[1])
+
+    def parsemoves(self,movestr):
+        self.moves=[[] for i in range(0,self.noplayers+1)]
+        if len(movestr)==0: return # no moves yet
+        movelist=movestr.split('),(')
+        movelist[0]=movelist[0][1:]
+        movelist[-1]=movelist[-1][:-1]
+        for m in movelist:
+            m=m.split(',')
+            mid=int(m[0])
+            x =int(m[1])
+            y =int(m[2])
+            self.moves[mid].append((x,y))
 
 if __name__=="__main__":
+    print "Get question from socket"
     # Protocol 2
     question=readsocket(s,1)
     assert (question=='Team Name?')
-    sendsocket(s, teamname)
-
+    sendsocket(s, "Briagonal")
     # Protocol 3, read and parse parameters
     params=readsocket(s)
     params=params.split(',')
-    #We assume there are two players
-    #We assume we have at most 15 stones to set
-    max_stones=int(params[1])
-    #Make sure the board dimensions match the standard
+    n_pl=int(params[0])
+    N=int(params[1])
     assert(int(params[2])==dim)
-    #We are either player 0 or player 1
-    our_pid=int(params[3]) - 1
-    #We expect there to be at most 2 players
-    assert(int(params[3]) in [1, 2])
+    pid=int(params[3])
+    state=State(n_pl, N, pid)
 
-    state=State()
     # Game phase 4
-    for turn in xrange(max_stones):
-        #OUR PLAYER CONVENTION IS DIFFERENT FROM THE PROTOCOL
-        for pl in range(2):
-            servstr = readsocket(s)
-            state.update_state(servstr)
-            if pl == our_pid:
-                input_args = []
-                for move in state.moves:
-                    if move:
-                        for x in move:
-                            input_args.append(str(x))
-                if (automatic):
-                    out = Popen(["./TEAM"] + input_args, stdout = PIPE)
-                    move = out.communicate()[0].split(" ")
+    for turn in xrange(N):
+        for pl in range(1,n_pl+1):
+            statestr=readsocket(s)
+            state.parsestate(statestr)
+            assert(pl==state.nextplayer)
+            if pl == pid:
+                #Calculate the id of the other player
+                oid = (pid + 1)
+                if oid == state.noplayers + 1:
+                    oid = 1
+                if state.moves[oid]:
+                    omove = state.moves[oid][-1]
                 else:
-                    move = raw_input("Input move string")
-                    move = move.split(" ")
-                print "Score: " + str(move[2])
-                makemove(s, our_pid + 1, int(move[0]), int(move[1]))
-
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-    
+                    omove = (500, 500)
+                mymove = (500-(omove[0] - 500), 500-(omove[1]-500))
+                #Should we select a greedy stone if the move already exists?
+                while (mymove in state.moves[oid]):
+                    mymove = (max(min(mymove[0] + random.randint(-2, 2), 1000), 0),
+                              max(min(mymove[1] + random.randint(-2, 2), 1000), 0))
+                makemove(s,pid,mymove[0],mymove[1])
+    state.parsestate(readsocket(s))
